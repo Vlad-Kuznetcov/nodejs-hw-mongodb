3870bfcb29dd3ce718f1eb10b85c54f66c05a755
Vlad <vladonik.909@gmail.com>
2024-10-23 19:32:07 +0300

    commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import { initMongoConnection } from './bd/initMongoConnection.js';
+import { setupServer } from './server.js';
+
+const boostrap = async () => {
+  await initMongoConnection();
+  setupServer();
+};
+
+boostrap();
